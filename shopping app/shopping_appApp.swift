561407f6da29086191c9065e8aa83e2304d37934
Nawo403 <thilinigunawardhana21@gmail.com>
2024-03-27 12:00:48 +0530

    Initial Commit

---
+++ shopping app/shopping_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_appApp.swift
+//  shopping app
+//
+//  Created by Thilini Gunawardhana on 2024-03-27.
+//
+
+import SwiftUI
+
+@main
+struct shopping_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
